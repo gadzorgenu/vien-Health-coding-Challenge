0fa9d2fc51bbe587fffd103b9550786470deccd4
Georgina <gadzorgenu2000>
2021-01-16 12:55:53 +0000

    Added graph

--- coding_challenge/src/components/Cards/DailyActiveCard.js
+++ coding_challenge/src/components/Cards/DailyActiveCard.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import CardHeader from './CardHeader'
-import { Box } from '@chakra-ui/react'
+import { Box, Image } from '@chakra-ui/react'
+import  graph2 from '../Assets/User Menu/Icons 24/graph2.png'
+
 
 const DailyActiveCard = ({mr}) => {
     return (
-        <Box borderRadius='10px' borderWidth={1} w={{ sm: "100% ", md:'100%',xl: '60%'}} mb={{ sm:5}} mr={{ md:12}}>
+        <Box borderRadius='10px' borderWidth={1} w={{ sm: "100% ", md:'100%',xl: '60%'}} padding={{ md:2}} mb={{ sm:5}} mr={{ md:12}}>
            <CardHeader 
            title='Daily active clients'
            />
+           <Image src={graph2} marginTop={{ md:6}}/>
        </Box>
     )
 }
--- coding_challenge/src/components/Cards/DailyProfitCard.js
+++ coding_challenge/src/components/Cards/DailyProfitCard.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import CardHeader from './CardHeader'
-import { Box } from '@chakra-ui/react'
+import { Box, Image } from '@chakra-ui/react'
+import  Graph1 from '../Assets/User Menu/Icons 24/Graph1.png'
 
 const DailyProfitCard = () => {
     
     return (
-        <Box borderRadius='10px' borderWidth={1} w={{ sm: "100% ", md:'100%',xl: '60%'}}  mb={{ sm:5}} mr={{ md:12}}>
+        <Box borderRadius='10px' borderWidth={1} w={{ sm: "100% ", md:'100%',xl: '70%'}} padding={{ md:2}}  mb={{ sm:5}} mr={{ md:12}}>
            <CardHeader 
                 title='Daily Profit'
                 my={{ md: 2}}
            />
+           <Image src={Graph1} marginTop={{ md:6}}/>
        </Box>
     )
 }
